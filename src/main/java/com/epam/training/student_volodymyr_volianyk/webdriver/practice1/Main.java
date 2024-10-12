00556541a5c3f54167bf3db1884d8719eff9583d
Volodymyr Volianyk <wrspct@gmail.com>
2024-10-13 00:54:10 +0300

    Initial commit

---
+++ src/main/java/com/epam/training/student_volodymyr_volianyk/webdriver/practice1/Main.java
@@ -0,0 +1,7 @@
+package com.epam.training.student_volodymyr_volianyk.webdriver.practice1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Test Automation");
+    }
+}
