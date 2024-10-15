427afa8a7af6d85e85b4d5fed1bcc9770d16c51e
Volodymyr Volianyk <wrspct@gmail.com>
2024-10-16 00:55:42 +0300

    Added Web driver

---
+++ src/test/java/HelloWebDriver.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HelloWebDriver {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev");
+        Thread.sleep(2000);
+        driver.quit();
+
+        driver = new EdgeDriver();
+        driver.get("https://www.selenium.dev");
+        Thread.sleep(2000);
+        driver.quit();
+
+        driver = new FirefoxDriver();
+        driver.get("https://www.selenium.dev");
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
