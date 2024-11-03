37e72bcf95a139184e7c19137a26c3db320a5f82
Volodymyr Volianyk <wrspct@gmail.com>
2024-11-03 15:01:15 +0200

    optimise import

--- src/test/java/com/epam/training/student_volodymyr_volianyk/final_task/page/SaucedemoLoginPage.java
+++ src/test/java/com/epam/training/student_volodymyr_volianyk/final_task/page/SaucedemoLoginPage.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
